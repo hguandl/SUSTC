7900f5f73f3c5f9aa4933a2483b0bbad06b20815
chenshijie <chrischen369@outlook.com>
2018-12-20 10:27:50 +0800

    add controller for other main pages

--- src/main/java/com/ooad/scriptpro/web/MainController.java
+++ src/main/java/com/ooad/scriptpro/web/MainController.java
@@ -10,8 +10,28 @@ public class MainController {
         System.out.println("call main controller");
         return "index";
     }
-    @GetMapping("/edit.html")
+    @GetMapping("/edit")
     public String editControl(){
         return "edit";
+    }
+    @GetMapping("/myscripts")
+    public String myscriptsControl(){
+        return "myscripts";
+    }
+    @GetMapping("/search")
+    public String searchControl(){
+        return "search";
+    }
+    @GetMapping("/signin")
+    public String signinControl(){
+        return "signin";
+    }
+    @GetMapping("/signup")
+    public String signupControl(){
+        return "signup";
+    }
+    @GetMapping("/welcome")
+    public String infoControl(){
+        return "welcome";
     }
 }
