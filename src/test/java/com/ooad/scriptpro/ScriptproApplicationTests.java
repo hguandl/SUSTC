61fb012eefcc70e45ea308a53dcb9f12043f84af
hguandl <hguandl@gmail.com>
2018-12-23 14:19:25 +0800

    stderr of docker

--- src/test/java/com/ooad/scriptpro/ScriptproApplicationTests.java
+++ src/test/java/com/ooad/scriptpro/ScriptproApplicationTests.java
@@ -1,8 +1,6 @@
 package com.ooad.scriptpro;
 
-import com.ooad.scriptpro.service.docker.Container;
 import com.ooad.scriptpro.service.docker.DockerLog;
-import com.ooad.scriptpro.service.docker.config.ScriptLang;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -19,9 +17,11 @@ public class ScriptproApplicationTests {
 	@Test
 	public void dockerLogParse() {
 		// "hello\nworld\n"
-		byte[] raw = {1, 0, 0, 0, 0, 0, 0, 12, 104, 101, 108, 108, 111, 10, 119, 111, 114, 108, 100, 10};
+		byte[] raw = {1, 0, 0, 0, 0, 0, 0, 12, 104, 101, 108, 108, 111, 10, 119, 111, 114, 108, 100, 10,
+			2, 0, 0, 0, 0, 0, 0, 2, 104, 10};
 		DockerLog log = new DockerLog(raw);
-		System.out.println(log);
+		System.out.println("output: " + log.getStdOut());
+		System.out.println("error: " + log.getStdErr());
 	}
 }
 
