f5462840df9ff26cd7f98cd3df633cc4135bf95c
RangerX-36917 <chrischen369@outlook.com>
2018-12-25 20:15:24 +0800

    add user to session
    Add user to session.
    Now login controller get user from the database instead of 'new' a new one each time.

--- src/main/java/com/ooad/scriptpro/web/UserController.java
+++ src/main/java/com/ooad/scriptpro/web/UserController.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.servlet.http.HttpSession;
 
@@ -35,17 +36,18 @@ public class UserController {
     public String login(@RequestParam String username,
                         @RequestParam String password,
                         HttpSession httpSession,
-                        Model model){
+                        Model model,
+                        RedirectAttributes redirectAttributes){
         System.out.println("call log in");
         System.out.println(username+"  "+password);
-        User user = new User();
-        user.setUsername(username);
-        user.setPassword(password);
+        User user = userDao.findUserByUsername(username);
         System.out.println("username: "+user.getUsername()+"  password: "+user.getPassword());
         boolean verify = loginService.verifyLogin(user);
         if(verify) {
             System.out.println("success");
             httpSession.setAttribute(WebSecurityConfig.SESSION_KEY, username);
+            httpSession.setAttribute("user",user);
+            redirectAttributes.addAttribute("user",user);
             return "redirect:/userHome";
         } else{
             System.out.println("fail");
@@ -55,10 +57,7 @@ public class UserController {
         }
 
     }
-    @GetMapping(value = {"userHome"})
-    public String userHome(){
-        return "/userHome";
-    }
+
     @GetMapping(value = {"/login","/login.html"})
     public String signinControl(){
         //model.addAttribute("message","")
