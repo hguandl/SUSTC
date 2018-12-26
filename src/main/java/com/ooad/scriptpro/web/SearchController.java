2513542c4f5217802dc3e226e6a552f47bdcd09d
tiankai <tiankai@tiankais-MacBook-Pro.local>
2018-12-26 15:42:05 +0800

    add search function

---
+++ src/main/java/com/ooad/scriptpro/web/SearchController.java
@@ -0,0 +1,27 @@
+package com.ooad.scriptpro.web;
+
+import com.ooad.scriptpro.model.Script;
+import com.ooad.scriptpro.service.ScriptService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import javax.servlet.http.HttpSession;
+import java.util.List;
+
+@Controller
+public class SearchController {
+    @Autowired
+    ScriptService scriptService;
+
+    @RequestMapping(value = "/search", method = RequestMethod.POST)
+    public String scriptController(Model model,
+                                   @RequestParam String str){
+        List<Script> scriptList = scriptService.vagueSearch(str);
+        model.addAttribute("scriptList", scriptList);
+        return "redirect:/search";
+    }
+}
