9ecbc911f62584082886c7952f560332b544b946
gongyue <1024344030@qq.com>
2018-12-27 02:28:28 +0800

    add run script

--- src/main/java/com/ooad/scriptpro/web/ScriptController.java
+++ src/main/java/com/ooad/scriptpro/web/ScriptController.java
@@ -2,25 +2,19 @@ package com.ooad.scriptpro.web;
 
 import ch.qos.logback.core.encoder.EchoEncoder;
 import com.fasterxml.jackson.databind.jsontype.TypeSerializer;
-import com.ooad.scriptpro.model.Script;
-import com.ooad.scriptpro.model.ScriptForm;
-import com.ooad.scriptpro.model.ScriptFormText;
-import com.ooad.scriptpro.model.User;
+import com.ooad.scriptpro.model.*;
 import com.ooad.scriptpro.service.FileService;
 import com.ooad.scriptpro.service.ScriptService;
 import com.ooad.scriptpro.service.TypeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpSession;
 import java.sql.Clob;
 
-@Controller
+@RestController
 public class ScriptController {
     @Autowired
     ScriptService scriptService;
@@ -79,6 +73,17 @@ public class ScriptController {
             return "Error! String to clob fail!";
         }
 
+    }
+
+    @PostMapping("/runScript/{id}/{type}")
+    public void runScript(@PathVariable int id, @PathVariable(name = "type") String typeName, @ModelAttribute(value = "scriptRun")ScriptRun scriptRun){
+
+        System.out.println(id);
+        System.out.println(typeName);
+        System.out.println(scriptRun.getArgs_str());
+        String res = scriptService.run(typeName, id, scriptRun.getArgs_str());
+        System.out.println("output:" + res);
+        // result = res;
     }
 
 
--- src/main/java/com/ooad/scriptpro/web/ScriptInfoController.java
+++ src/main/java/com/ooad/scriptpro/web/ScriptInfoController.java
@@ -1,11 +1,13 @@
 package com.ooad.scriptpro.web;
 
 import com.ooad.scriptpro.model.Script;
+import com.ooad.scriptpro.model.ScriptRun;
 import com.ooad.scriptpro.service.ScriptService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import javax.servlet.http.HttpSession;
@@ -16,13 +18,17 @@ public class ScriptInfoController {
     ScriptService scriptService;
 
     @GetMapping(value = "/info")
+    @ModelAttribute
     public String scriptController(Model model,
                                    HttpSession httpSession,
                                    @RequestParam long sid){
 //        System.out.println(sid);
         Script s = scriptService.findById(sid);
+        ScriptRun scriptRun = new ScriptRun();
 
         model.addAttribute("script", s);
+        model.addAttribute("scriptRun", scriptRun);
+        model.addAttribute("result", "");
         return "/info";
     }
 }
